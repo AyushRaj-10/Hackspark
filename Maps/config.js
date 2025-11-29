3bfb6edf911c9041a8eb8ad19abd84ae6047d4e6
varun <varunhackthons@gmail.com>
2025-11-29 18:26:28 +0530

    fresh update

--- Maps/config.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = 'KypXe5vRZNCTAOogXPkX1XmRGrxtPry2';
-export const OTD_API_URL = 'https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb';
-export const UPDATE_INTERVAL = 15000; // 15 seconds
-
